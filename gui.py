24c69a47283f924cccd7192dc332fc85f1b5f00f
Colin <arrowsmith.colin@gmail.com>
2019-12-05 09:36:29 -0500

    Created three buttons and embedded image GUI

--- gui.py
+++ gui.py
@@ -1,26 +1,65 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QDialog, QVBoxLayout, QGridLayout, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, Qt
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QWidget):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
+        # self.imageWidget = pg.ImageView()
+        # self.setCentralWidget(self.imageWidget)
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.initUI()
 
-        # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
+    def initUI(self) :
+        # --- WINDOW --- #
+        self.setWindowTitle("Image Labelling")
+        # Define the distance from top left of screen
+        # (first two ints), x,y size of windows (last two ints)
+        self.setGeometry(300, 400, 700, 500)
+
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+        # --- ###### --- #
+
+        # --- BUTTONS --- #
+        s_button = QPushButton("Strong")
+        w_button = QPushButton("Weak")
+        n_button = QPushButton("None")
+        s_button.setToolTip('Label this patient as having a strong artifact.')
+        w_button.setToolTip('Label this patient as having a weak artifact.')
+        n_button .setToolTip('Label this patient as having no artifacts.')
+        hbox.addStretch()
+        hbox.addWidget(s_button)
+        hbox.addWidget(w_button)
+        hbox.addWidget(n_button)
+        hbox.addStretch()
+        # --- ###### --- #
+
+        # --- TEXT   --- #
+        self.text_header = QLabel("Press [ENTER] to begin.")
+        self.text_header.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(self.text_header)
+
+
+        # --- IMAGE --- #
+        self.imageWidget = pg.ImageView()
+        vbox.addWidget(self.imageWidget)
+        # --- ----- --- #
+
+
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     main = MainWindow()
     main.show()
     sys.exit(app.exec_())
