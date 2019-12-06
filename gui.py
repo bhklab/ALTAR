0b4ac60b87a34479e26bdc169f5c88847dcc364e
CArrowsm <arrowsmith.colin@gmail.com>
2019-12-06 11:44:44 -0500

    Added style sheet

--- gui.py
+++ gui.py
@@ -22,6 +22,7 @@ class MainWindow(QWidget):
         # Define the distance from top left of screen
         # (first two ints), x,y size of windows (last two ints)
         self.setGeometry(300, 400, 700, 500)
+        self.setStyleSheet(open('style.css').read())
 
         hbox = QHBoxLayout()
         vbox = QVBoxLayout()
@@ -33,18 +34,27 @@ class MainWindow(QWidget):
         n_button = QPushButton("None")
         s_button.setToolTip('Label this patient as having a strong artifact.')
         w_button.setToolTip('Label this patient as having a weak artifact.')
-        n_button .setToolTip('Label this patient as having no artifacts.')
+        n_button.setToolTip('Label this patient as having no artifacts.')
+        s_button.setObjectName("label-slice")
+        w_button.setObjectName("label-slice-center")
+        n_button.setObjectName("label-slice")
+        s_button.clicked.connect(lambda: self.on_click(result="Strong"))
+        w_button.clicked.connect(lambda: self.on_click(result="Weak"))
+        n_button.clicked.connect(lambda: self.on_click(result="None"))
+
         hbox.addStretch()
         hbox.addWidget(s_button)
         hbox.addWidget(w_button)
         hbox.addWidget(n_button)
+        hbox.setSpacing(0)
         hbox.addStretch()
         # --- ###### --- #
 
-        # --- TEXT   --- #
+        # ---  TEXT   --- #
         self.text_header = QLabel("Press [ENTER] to begin.")
         self.text_header.setAlignment(Qt.AlignCenter)
         vbox.addWidget(self.text_header)
+        # --- ####### --- #
 
 
         # --- IMAGE --- #
@@ -56,7 +66,8 @@ class MainWindow(QWidget):
         vbox.addLayout(hbox)
         self.setLayout(vbox)
 
-
+    def on_click(self, result=None) :
+        print(result)
 
 def main():
     app = QApplication(sys.argv)
