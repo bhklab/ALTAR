9e36ecb7f5fcce046b04915c52855f7c06b3cd5a
CArrowsm <arrowsmith.colin@gmail.com>
2019-12-08 22:49:15 -0500

    Image viewer now part of a GUI window with buttons.

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QDialog, QVBoxLayout, QGridLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QWidget, QPushButton, QHBoxLayout, QDialog, QVBoxLayout, QGridLayout, QLabel
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 from pyqtgraph import PlotWidget, plot
@@ -6,23 +6,34 @@ import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 
+import numpy as np
+from paramiko import SSHClient
+from getpass import getpass
+
+
 class MainWindow(QWidget):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        # self.imageWidget = pg.ImageView()
-        # self.setCentralWidget(self.imageWidget)
-
-        self.initUI()
-
-    def initUI(self) :
-        # --- WINDOW --- #
-        self.setWindowTitle("Image Labelling")
         # Define the distance from top left of screen
         # (first two ints), x,y size of windows (last two ints)
         self.setGeometry(300, 400, 700, 500)
         self.setStyleSheet(open('style.css').read())
+
+        # Open remote connection to H4H
+        # If this is successful,
+        self.init_authentiation()
+
+        # Initialize the user interface
+        # self.initUI()
+
+        # Plot Initial image
+        # self.load_img("/cluster/home/carrowsm/temp_data/trg_data/1.npy")
+
+    def initUI(self) :
+        # --- WINDOW --- #
+        self.setWindowTitle("Image Labelling")
 
         hbox = QHBoxLayout()
         vbox = QVBoxLayout()
@@ -62,12 +73,78 @@ class MainWindow(QWidget):
         vbox.addWidget(self.imageWidget)
         # --- ----- --- #
 
-
         vbox.addLayout(hbox)
         self.setLayout(vbox)
 
+
     def on_click(self, result=None) :
         print(result)
+
+    def init_authentiation(self) :
+        # We are currently in "authentication mode"
+        self.mode = "auth"
+
+        # Set GUI title
+        self.setWindowTitle("H4H Login")
+
+        # Create a widget with username and password fields
+        vbox = QVBoxLayout()
+        user = QLineEdit()
+        pw = QLineEdit()
+        submit_button = QPushButton("Login")
+        submit_button.setObjectName("login")
+        submit_button.clicked.connect(lambda: self.authenticate(user.text(), pw.text(), vbox))
+        pw.setEchoMode(QLineEdit.Password)
+        user.setPlaceholderText("Username")
+        pw.setPlaceholderText("Password")
+
+        # Prompt user for their h4h cridentials
+        vbox.addStretch()
+        vbox.addWidget(user)
+        vbox.addWidget(pw)
+        vbox.addWidget(submit_button)
+        vbox.addStretch()
+        self.setLayout(vbox)
+
+    def authenticate(self, username, password, auth_widget) :
+        # Remove the authentication widget
+        QWidget().setLayout(auth_widget.layout())
+        try :
+            self.sftp = self.setup_remote(username, password)
+            print("Authentication Successful")
+            self.initUI()
+        except :
+            self.init_authentiation()
+            print("Authentication Unsuccessful")
+
+
+    def setup_remote(self, username, password) :
+        host = "172.27.23.173"
+        port = 22
+
+        ssh = SSHClient()
+        ssh.load_system_host_keys()
+        ssh.connect(host, port=port,
+                    username=username, password=password)
+        sftp_client = ssh.open_sftp()
+        return sftp_client
+
+    def load_img(self, path) :
+        remote_file = self.sftp_client.open(path)
+        X = np.load(remote_file)
+        print(X.shape)
+        self.imageWidget.setImage(X)
+
+    def keyPressEvent(self, e):
+        """ Handle key press events"""
+        if e.key == "16777220" :
+            # Enter key was hit
+            if self.mode == "auth" :
+                print("Authenticating")
+            if self.mode == "label" :
+                print("")
+
+
 
 def main():
     app = QApplication(sys.argv)
